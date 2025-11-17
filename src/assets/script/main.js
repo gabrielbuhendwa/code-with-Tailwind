06b2dab0ad46165855c061fd573386d0d2ecf2ef
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-17 22:34:14 +0200

    add this to my repository

--- src/assets/script/main.js
+++ src/assets/script/main.js
@@ -1,34 +1,40 @@
-//menu icon toggle
 window.onload = () => {
-    const mobileMenuIcon = document.getElementById("mobile-menu-button");
-    mobileMenuIcon.addEventListener("click", () => {
-        if (mobileMenuIcon.classList.contains("open")) {
+        const mobileMenuIcon = document.getElementById("mobile-menu-button");
+        mobileMenuIcon.addEventListener("click", () => {
+          if (mobileMenuIcon.classList.contains("open")) {
             mobileMenuIcon.classList.remove("open");
-        }else{
+          } else {
             mobileMenuIcon.classList.add("open");
           }
         });
 
-//tickets menu toggle
-    const ticketsMenuItem = document.getElementById("ticket-menu-item");
-    ticketsMenuItem.addEventListener("click", () => {
-        if (ticketsMenuItem.classList.contains("open")) {
+        const ticketsMenuItem = document.getElementById("ticket-menu-item");
+        ticketsMenuItem.addEventListener("click", () => {
+          if (ticketsMenuItem.classList.contains("open")) {
             ticketsMenuItem.classList.remove("open");
-        }else{
+          } else {
             ticketsMenuItem.classList.add("open");
-        }
-    });
+          }
+        });
 
+        const darkModeToggle = document.getElementById("dark-mode-toggle");
+        darkModeToggle.addEventListener("click", () => {
+          if (document.body.classList.contains("dark")) {
+            document.body.classList.remove("dark");
+          } else {
+            document.body.classList.add("dark");
+          }
+        });
 
-//const previousButton = document.getElementById("previous");
-    const nextButton = document.getElementById("next");
-    const carousel = document.getElementById("carousel");
-    previousButton.addEventListener("click", () => {
-        const itemWidth = carousel.getElementsByTagName("div")[0].clientWidth;
-        carousel.scrollLeft = carousel.scrollLeft - itemWidth;
-    });
-    nextButton.addEventListener("click", () => {
-        const itemWidth = carousel.getElementsByTagName("div")[0].clientWidth;
-        carousel.scrollLeft = carousel.scrollLeft + itemWidth;
-    });    
-};
+        const previousButton = document.getElementById("previous");
+        const nextButton = document.getElementById("next");
+        const carousel = document.getElementById("carousel");
+        previousButton.addEventListener("click", () => {
+          const itemWidth = carousel.getElementsByTagName("div")[0].clientWidth;
+          carousel.scrollLeft = carousel.scrollLeft - itemWidth;
+        });
+        nextButton.addEventListener("click", () => {
+          const itemWidth = carousel.getElementsByTagName("div")[0].clientWidth;
+          carousel.scrollLeft = carousel.scrollLeft + itemWidth;
+        });
+      };
